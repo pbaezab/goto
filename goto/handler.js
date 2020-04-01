60988656b80e9525a660515bda7373e4c0bc35de
Pablo Baeza <pbaezab@falabella.cl>
2020-04-01 14:56:36 -0300

    Have fun
    Signed-off-by: Pablo Baeza <pbaezab@falabella.cl>

--- goto/handler.js
+++ goto/handler.js
@@ -4,7 +4,7 @@ module.exports = (event, context) => {
     let redirect;
 
     if(event.path == "/home") {
-        redirect = "https://www.google.cl/";
+        redirect = "https://www.google.com/";
     } else if(event.path == "/sponsors" || event.path == "/insiders") {
         redirect = "https://github.com/users/alexellis/sponsorship";
     } else if(event.path == "/inlets") {
